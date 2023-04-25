612318e57bd9a023384feb25ed54d71c7b6773c2
Frvcq <90377092+Frvcq@users.noreply.github.com>
2023-04-25 14:00:18 +0200

    feat

---
+++ exercice2.ps1
@@ -0,0 +1,30 @@
+# Récupérer tous les utilisateurs et leurs propriétés de département et de ville
+$users = Get-ADUser -Filter * -Properties Department, City
+
+# Identifier l'ensemble des villes et des services d'appartenance des utilisateurs
+$cities = $users | Select-Object -ExpandProperty City -Unique
+$departments = $users | Select-Object -ExpandProperty Department -Unique
+
+# Créer des OU et des groupes en fonction des propriétés de département et de ville de chaque utilisateur
+foreach ($city in $cities) {
+   New-ADOrganizationalUnit -Name $city -Path "DC=ad01,DC=lcl"
+  foreach ($department in $departments) {
+
+   New-ADOrganizationalUnit -Name $department -Path "OU=$city,DC=ad01,DC=lcl"
+
+    New-ADGroup -Name "$department $city" -GroupScope Global -Path "OU=$department,OU=$city,DC=ad01,DC=lcl"
+
+    $deptGroup = Get-ADGroup -Filter { Name -eq "$department $city"} | Select-Object -Property DistinguishedName
+    Write-Host $deptGroup "//////////////"
+    
+    $usersInDept = $users | Where-Object { $_.City -eq $city -and $_.Department -eq $department }
+
+    foreach ($user in $usersInDept) {
+    Write-Host $deptGroup "//////////////"
+      Add-ADGroupMember -Identity $deptGroup -Members $user
+    }
+  
+  }
+
+
+}
